41d5775cdcc7d10ae7558a00928dba39a1c8ea8f
shooug95-prog <shooug95@gmail.com>
2026-08-14 14:00:13 +0300

    Update server.js
    Update server for deployment

--- server.js
+++ server.js
@@ -71,6 +71,8 @@ app.post("/feedback", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`💜 نظام تقييم صالون لمسة نونه يعمل على المنفذ ${PORT}`);
+})
